96798c3817d5beffbb48159a3d37afa28b2c3f85
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-03-03 19:22:06 +0000

    Correct state replace command, revert execute

--- lib/cp_env/terraform.rb
+++ lib/cp_env/terraform.rb
@@ -90,7 +90,7 @@ class CpEnv
     #terraform state replace-provider registry.terraform.io/-/pingdom registry.terraform.io/russellcardullo/pingdom
     def tf_state_replace
       if terraform_executable.match(/terraform0.13/) &&  Dir.glob("#{tf_dir}/*pingdom*.tf").size>0
-        cmd = "#{terraform_executable} state replace-provider registry.terraform.io/-/pingdom registry.terraform.io/russellcardullo/pingdom"
+        cmd = "#{terraform_executable} state replace-provider -auto-approve registry.terraform.io/-/pingdom registry.terraform.io/russellcardullo/pingdom"
         log("blue", "replacing pingdom provider for namespace #{namespace} in #{cluster}") 
         execute("cd #{tf_dir}; #{cmd}")
       end
