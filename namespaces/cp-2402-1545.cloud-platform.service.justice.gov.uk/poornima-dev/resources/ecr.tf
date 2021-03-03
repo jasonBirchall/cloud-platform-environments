83dcacca0a669a86904701c3f7f2c73300d96d63
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-03-03 19:31:48 +0000

    Revert module version upgrade

--- namespaces/cp-2402-1545.cloud-platform.service.justice.gov.uk/poornima-dev/resources/ecr.tf
+++ namespaces/cp-2402-1545.cloud-platform.service.justice.gov.uk/poornima-dev/resources/ecr.tf
@@ -5,7 +5,7 @@
  *
  */
 module "cp_team_test_ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.4"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.3"
   repo_name = "test-demo"
   team_name = "test-team"
 
